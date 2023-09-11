444792b2acb7622adffbd62ebbd5faa688444536
cesar <juliocesar3653@gmail.com>
2023-09-11 06:31:58 -0500

    Exercise 2

--- prueba.sh
+++ prueba.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/big-data-europe/docker-hadoop
-
+# git clone https://github.com/big-data-europe/docker-hadoop
 # Aqui levantamos hadoop
 sudo docker compose up
 
@@ -18,17 +17,21 @@ export HADOOP_CLASSPATH=$(hadoop classpath)
 hadoop fs -mkdir /Input
 # Esto no es necesario
 # hadoop fs -mkdir /Output
-hadoop fs -put /tmp/WordCount.java /Input
+# hadoop fs -put /tmp/WordCount.java /Input
 hadoop fs -put /tmp/input.txt /Input
+SalesJan2009.csv
 
 mkdir classes
 
 javac -classpath $(hadoop classpath) -d ./classes/ ./tmp/WordCount.java
+# javac -classpath $(hadoop classpath) -d ./classes/ ./tmp/SalesCountry/SalesCountryDriver.java ./tmp/SalesCountry/SalesCountryReducer.java ./tmp/SalesCountry/SalesMapper.java
 
 # Esto genera el .jar
 jar -cvf WordCount.jar -C ./classes .
+# jar -cvf SalesCountryDriver.jar -C ./classes .
 
 hadoop jar WordCount.jar WordCount /Input/input.txt /Output
+# hadoop jar SalesCountryDriver.jar SalesCountryDriver /Input/SalesJan2009.csv /Output
 
 hadoop job -list all
 
